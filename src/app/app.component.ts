2e5dc07323acf00ffe80d5eff2f717815b516e30
shubham jain <shubhamjain2908>
2018-03-23 03:15:31 +0530

    Template-Driven approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') submitForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // in this approach you have to override the complete form data
+    // this.submitForm.setValue({
+    //   userData : {
+    //     username : suggestedName,
+    //     email : 'shubhamjain2908@gmail.com'
+    //   },
+    //   secret: '',
+    //   questionAnswer: 'AAAAAAAOOOOOOOOOOOOOAAAAAAAAAAAOOOOOOOO',
+    //   gender: 'male'
+    // });
+    this.submitForm.form.patchValue({
+      userData: {username: suggestedName}
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.submitForm.value.userData.username;
+    this.user.email = this.submitForm.value.userData.email;
+    this.user.secretQuestion = this.submitForm.value.secret;
+    this.user.answer = this.submitForm.value.questionAnswer;
+    this.user.gender = this.submitForm.value.gender;
+
+    this.submitForm.reset();
   }
 }
