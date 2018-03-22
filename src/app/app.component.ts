3c9072543de4583178cd4dd32a3d93240826f181
shubham jain <shubhamjain2908>
2018-03-23 00:51:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  featureLoaded = 'recipe';
-
-  onNavigate(feature: string) {
-    this.featureLoaded = feature;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
