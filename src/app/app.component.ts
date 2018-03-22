46623a1c39d856d33c44e8ad2674b2a7a34098ae
shubham jain <shubhamjain2908>
2018-03-23 03:24:42 +0530

    initial commit: Reactive approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,50 +6,5 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('f') submitForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  submitted = false;
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-
-    // in this approach you have to override the complete form data
-    // this.submitForm.setValue({
-    //   userData : {
-    //     username : suggestedName,
-    //     email : 'shubhamjain2908@gmail.com'
-    //   },
-    //   secret: '',
-    //   questionAnswer: 'AAAAAAAOOOOOOOOOOOOOAAAAAAAAAAAOOOOOOOO',
-    //   gender: 'male'
-    // });
-    this.submitForm.form.patchValue({
-      userData: {username: suggestedName}
-    });
-  }
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
-
-  onSubmit() {
-    this.submitted = true;
-    this.user.username = this.submitForm.value.userData.username;
-    this.user.email = this.submitForm.value.userData.email;
-    this.user.secretQuestion = this.submitForm.value.secret;
-    this.user.answer = this.submitForm.value.questionAnswer;
-    this.user.gender = this.submitForm.value.gender;
-
-    this.submitForm.reset();
-  }
 }
