a904219449e71d51539d26c29772dc847a4698bd
shubham jain <shubhamjain2908>
2018-03-24 00:19:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,50 +6,26 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('f') submitForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  submitted = false;
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-
-    // in this approach you have to override the complete form data
-    // this.submitForm.setValue({
-    //   userData : {
-    //     username : suggestedName,
-    //     email : 'shubhamjain2908@gmail.com'
-    //   },
-    //   secret: '',
-    //   questionAnswer: 'AAAAAAAOOOOOOOOOOOOOAAAAAAAAAAAOOOOOOOO',
-    //   gender: 'male'
-    // });
-    this.submitForm.form.patchValue({
-      userData: {username: suggestedName}
+  servers = [
+    {
+      name: 'Testserver',
+      capacity: 10,
+      id: this.generateId()
+    },
+    {
+      name: 'Liveserver',
+      capacity: 100,
+      id: this.generateId()
+    }
+  ];
+  onAddServer(name: string) {
+    this.servers.push({
+      name: name,
+      capacity: 50,
+      id: this.generateId()
     });
   }
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
-
-  onSubmit() {
-    this.submitted = true;
-    this.user.username = this.submitForm.value.userData.username;
-    this.user.email = this.submitForm.value.userData.email;
-    this.user.secretQuestion = this.submitForm.value.secret;
-    this.user.answer = this.submitForm.value.questionAnswer;
-    this.user.gender = this.submitForm.value.gender;
-
-    this.submitForm.reset();
+  private generateId() {
+    return Math.round(Math.random() * 10000);
   }
 }
