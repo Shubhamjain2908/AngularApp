54fdeed328cd2b89dda501deaa90b849b2268e33
shubham jain <shubhamjain2908@gmail.com>
2018-05-20 20:43:40 +0530

    Adding Component Splitting repo in new Branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedFeature = 'recipe';
+export class AppComponent {
+  serverElements = [];
 
-  ngOnInit() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyBds8OPzSUkclIGB060hvwRYwE0aQHUXCg",
-      authDomain: "angularapp-http.firebaseapp.com"
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
   }
+
 }
